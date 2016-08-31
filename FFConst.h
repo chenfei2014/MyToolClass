6f83941dd5cc4f381ba514241d1abd6c541d3fe3
chenfei <chenfe.i@163.com>
2016-08-31 18:56:48 +0800

    常量文件

---
+++ FFConst.h
@@ -0,0 +1,14 @@
+// 常量定义文件
+#import <UIKit/UIKit.h>
+
+#pragma mark - 常量
+UIKIT_EXTERN NSString *const channelId;
+
+UIKIT_EXTERN CGFloat const FFTabBarHeight;
+UIKIT_EXTERN CGFloat const FFNavigationBarHeight;
+
+#pragma mark - 通知
+UIKIT_EXTERN NSString * const FFTopicMyGroupDidSelectNotification;
+UIKIT_EXTERN NSString * const FFSelectTopicMyGroupKey;
+
+
---
+++ FFConst.m
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+#pragma mark - 常量
+NSString *const channelId = @"guanwang";
+
+CGFloat const FFTabBarHeight = 49;
+CGFloat const FFNavigationBarHeight = 64;
+
+#pragma mark - 通知
+NSString *const FFTopicMyGroupDidSelectNotification = @"FFTopicMyGroupDidSelectNotification";
+NSString *const FFSelectTopicMyGroupKey = @"FFSelectTopicMyGroupKey";
